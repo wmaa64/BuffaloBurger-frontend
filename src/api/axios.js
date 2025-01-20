e1b74ac9299168720c6992139a067d2655afa656
wael mohammed aly <wmaa64@yahoo.com>
2025-01-20 15:34:46 +0200

    4th 20-1-25

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,7 +4,7 @@ const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5000'  
   //baseURL: 'https://ecommerce-backend-z4v3.onrender.com'
   //baseURL: 'https://buffaloburger-backend-production.up.railway.app'
-  baseURL: 'https://buffaloburger-backend.glitch.me'
+  baseURL: 'https://buffaloburger-backend-20251.fly.dev/'
 });
  
 export default axiosInstance;
